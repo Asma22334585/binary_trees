466155922ecc06f2cba9a0842d309c28f7b38ce6
Asma22334585 <guesmihadil0@gmail.com>
2024-02-27 15:13:07 -0800

    my commit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,8 +11,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+
 	r_height = 1 + binary_tree_height(tree->right);
 	l_height = 1 + binary_tree_height(tree->left);
 
-	return (left_height > right_height ? left_height : right_height);
+	if (l_height > r_height)
+		return (l_height);
+	return (r_height);
 }
