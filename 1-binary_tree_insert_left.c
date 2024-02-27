951004f6575a4579323391cd7805d8485bd07a6b
Asma22334585 <guesmihadil0@gmail.com>
2024-02-27 13:39:31 -0800

    my commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,6 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->n = value;
 	new_node->right = NULL;
 	new_node->left = NULL;
+	return (new_node);
 
 	if (parent->left == NULL)
 		parent->left = new_node;
