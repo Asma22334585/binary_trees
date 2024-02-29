7e71ca15885434a1f8d4239fcb16943a109d664f
Asma22334585 <guesmihadil0@gmail.com>
2024-02-28 17:09:59 -0800

    my commit

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node)
 		return (NULL);
-	if (!node->parent->parent->left || !node->parent->parent->right)
+	if (!node->parent || !node->parent->parent)
 		return (NULL);
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
