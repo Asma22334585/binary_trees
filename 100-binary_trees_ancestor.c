5385e4cf22ee7bef27ec5ef68649baf5982a8e2b
Asma22334585 <guesmihadil0@gmail.com>
2024-02-29 13:45:41 -0800

    my commit

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -41,8 +41,10 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		second = second->parent;
 		s--;
 	}
-	while (first == second)
+	while (first && second)
 	{
+		if (first == second)
+			return ((binary_tree_t *)first);
 		first = first->parent;
 		second = second->parent;
 	}
