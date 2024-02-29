bcf888ab41eb214cbb5d17fdc8d0fdd6118d3c3c
Asma22334585 <guesmihadil0@gmail.com>
2024-02-28 17:05:56 -0800

    my commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,11 +29,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int l, r;
-
 	if (!tree)
 		return (0);
-	l = binary_tree_height(tree->left);
-	r = binary_tree_height(tree->right);
-	return (l - r);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
