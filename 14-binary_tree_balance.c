a8ce42466dca2411c0e0eb1178a22beb504dfbc7
Asma22334585 <guesmihadil0@gmail.com>
2024-02-28 16:25:29 -0800

    my commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,21 +23,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_leaf -  checks if a node is a leaf
- * @node: pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
- */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (!node)
-		return (0);
-	if (node->left != NULL || node->right != NULL)
-		return (0);
-	else
-		return (1);
-}
-
-/**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
  * Return: balance factor
