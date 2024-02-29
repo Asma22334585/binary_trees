8c78e5ff1bb8f667756524fd3bdc200f8043feaf
Asma22334585 <guesmihadil0@gmail.com>
2024-02-29 12:02:46 -0800

    my commit

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -6,17 +6,16 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	if (tree == NULL)
+	binary_tree_t *new_root;
+
+	if (!tree || !tree->right)
 		return (NULL);
-	tree->parent = tree->right;
-	if (tree->right)
-	{
-		tree->right = tree->right->left;
-		tree->parent->left = tree;
-		tree->parent->parent = NULL;
-		if (tree->right)
-			tree->right->parent = tree;
-		return (tree->parent);
-	}
-	return (tree);
+	new_root = tree->right;
+	tree->right = new_root->left;
+	if (new_root->left != NULL)
+		new_root->left->parent = tree;
+	new_root->left = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
+	return (new_root);
 }
